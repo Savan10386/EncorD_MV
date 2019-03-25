5714eccb2706f666c226fc15e785b8c981c43aaa
Mahesh wagh <48907633+maheshwagh2988@users.noreply.github.com>
2019-03-25 18:37:50 +0530

    Update Mahesh Driver Path
    Test

--- src/main/java/EncorDashboard/GlobalLibrary/GlobalFunction.java
+++ src/main/java/EncorDashboard/GlobalLibrary/GlobalFunction.java
@@ -21,7 +21,7 @@ public class GlobalFunction {
 	public static void BrowsersSettings()
 	{
 		//office
-		File file = new File("C:/Users/Savan/git/EncorD_MV/chromedriver.exe");
+		File file = new File("C:/Users/leomahesh/git/EncorD_MV/chromedriver.exe");
 		//home
 		//File file = new File("C:/Users/leomahesh/git/EncorD-Dashboard/Library/chromedriver.exe"); 
 		
